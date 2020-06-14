ee28840405cc9dffd704459b0883a1350d1a8019
worldgu <15893851179@163.com>
2020-06-14 23:53:28 +0800

    测试代码

--- javase/src/test/java/com/zero/main/DemoTest.java
+++ javase/src/test/java/com/zero/main/DemoTest.java
@@ -1,6 +1,11 @@
 package com.zero.main;
 
+import lombok.extern.java.Log;
 import org.junit.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
 
 /**
  *@ClassName Test
@@ -8,6 +13,7 @@ import org.junit.Test;
  *@Author 张春海
  *@Date 2020/6/12 10:56 
  *@Version 1.0 */
+@Log
 public class DemoTest {
 
     @Test
@@ -45,8 +51,8 @@ public class DemoTest {
 ////        float fa[][] = new float[][];
 ////        float [6][]fs = new float[6][6];
 //        float [][]fss = new float[6][];
-int ab = 1;
-        Integer as = new Integer("asda");  //  其中有一个格式转换
+        int ab = 1;
+        Integer as = new Integer("666");  //  其中有一个格式转换
 //        System.out.println(">>>>>> " + as);
 
         //
@@ -57,5 +63,60 @@ int ab = 1;
     static void mb_operate(StringBuffer x, StringBuffer y){
         x.append(y);
         y = x;
+    }
+
+
+    @Test
+    public void TestCalendar(){
+        Calendar as = Calendar.getInstance();
+        System.out.println(">>>>" + as.getTimeZone());
+        System.out.println(">>>>" + as.getTimeZone());
+        System.out.println(">>>>" + as.getTimeZone());
+    }
+
+    @Test
+    public void TestString(){
+        String s = "story";
+//        char c = s[1];
+        int lent = s.length();
+//        String t = 100;
+        System.out.println(">>>>" + (s+="books"));
+    }
+
+    String str = new String("good");
+    char[] ch = { 'a', 'b', 'c' };
+
+    @Test
+    public void TestExcample() {
+        DemoTest ex = new DemoTest();
+        ex.change(ex.str, ex.ch);
+        System.out.print(ex.str + "and");
+        System.out.print(ex.ch);
+    }
+
+    public void change(String str, char ch[]) {
+        str = "test ok";
+        ch[0] = 'g';
+    }
+
+    @Test
+    public void TestFile(){
+        String str ="文件写入练习";
+        FileWriter fw = null;        //1
+        try{
+            fw = new FileWriter("c:mytext.txt");  //2
+//            fw.writerToEnd(str);   //3
+        }catch(IOException e){   //4
+            e.printStackTrace();
+        }finally{
+            //此处省略关闭流
+        }
+    }
+
+    @Test
+    public void TestShuZu(){
+        int[][] t={{1,2,3},{4,5,6}};
+        System.out.print( t.length + ">>>>>>>>" + t[0].length);
+
     }
 }
