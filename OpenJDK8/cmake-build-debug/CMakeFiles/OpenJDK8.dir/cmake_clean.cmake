ddede86308d54db5dd71cb00959798bc77496bca
worldgu <15893851179@163.com>
2020-08-03 22:33:36 +0800

    上传

---
+++ OpenJDK8/cmake-build-debug/CMakeFiles/OpenJDK8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenJDK8.dir/main.c.obj"
+  "OpenJDK8.exe"
+  "OpenJDK8.exe.manifest"
+  "OpenJDK8.lib"
+  "OpenJDK8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OpenJDK8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OpenJDK8/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int zhang(){
+    printf("zhanghcun");
+    return 9;
+}
+int main() {
+    printf("Hello, World!\n");
+    zhang();
+    return 0;
+}
+
