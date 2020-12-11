6a9052eb9b51a007329f0457bc6e77e32dd449e1
worldgu <15893851179@163.com>
2020-12-11 23:49:15 +0800

    javalambda相关

---
+++ lockdemo/src/main/java/com/zero/lockdemo/LockdemoApplication.java
@@ -0,0 +1,13 @@
+package com.zero.lockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockdemoApplication.class, args);
+	}
+
+}
